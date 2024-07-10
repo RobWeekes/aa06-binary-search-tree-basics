0af86bda18c39efff4b50bb73ff4e239250e4a70
RobWeekes <157428964+RobWeekes@users.noreply.github.com>
2024-07-10 15:59:22 -0700

    insert done - passing 7/14 - added comments

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -10,40 +10,64 @@ class TreeNode {
 class BinarySearchTree {
 
   constructor() {
-    // Your code here 
+      this.root = null;
   }
 
-  insert(val, currentNode=this.root) {
-    // Your code here 
+  insert(val, currentNode = this.root) {
+      const newNode = new TreeNode(val);
+
+      if(!currentNode) { // for empty tree, insert first node as root
+          this.root = newNode;
+          return;
+      }
+
+      if(val < currentNode.val) {   // val is smaller than node at root <- go down left leg
+        // check if left node is empty --
+          if(currentNode.left === null) {   // if left node empty / if(!currentNode.left)
+              currentNode.left = newNode;   // set new node to root's left node
+          } else {   // if left node is filled, traverse down left & recurse evaluate
+              this.insert(val, currentNode.left);
+          }
+      }
+
+      if(val > currentNode.val) {   // val is larger than node at root -> go down right leg
+          // check if right node is empty
+          if(currentNode.right === null) {   // if right is empty
+              currentNode.right = newNode;   // set right node as new node
+          } else {   // right node filled? recurse down the right
+              this.insert(val, currentNode.right);
+          }
+      }
   }
+
 
   search(val) {
-    // Your code here 
+    // Your code here
   }
 
 
   preOrderTraversal(currentNode = this.root) {
-    // Your code here 
+    // Your code here
   }
 
 
   inOrderTraversal(currentNode = this.root) {
-    // Your code here 
+    // Your code here
   }
 
 
   postOrderTraversal(currentNode = this.root) {
-    // Your code here 
+    // Your code here
   }
 
     // Breadth First Traversal - Iterative
   breadthFirstTraversal() {
-    // Your code here 
+    // Your code here
   }
 
   // Depth First Traversal - Iterative
   depthFirstTraversal() {
-    // Your code here 
+    // Your code here
   }
 }
 
